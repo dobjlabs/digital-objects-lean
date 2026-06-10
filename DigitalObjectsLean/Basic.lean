ca864009e2ed5a2262a392cd34b68f7f81ef37d2
Eduard S. <eduardsanou@posteo.net>
2026-06-10 16:54:14 +0200

    complete initial spec

--- DigitalObjectsLean/Basic.lean
+++ DigitalObjectsLean/Basic.lean
@@ -5,10 +5,12 @@ import Mathlib.Data.Set.Basic
 -- using undefined symbols
 set_option autoImplicit false
 
+-- Type: Observable effect on the system state
 inductive Effect {Object : Type} where
   | create (o : Object)
   | consume (o : Object)
 
+-- Type: Operation affecting objects (symbolic or concrete)
 inductive Op (α : Type) where
   | insert (x : α)
   | delete (x : α)
@@ -17,103 +19,105 @@ inductive Op (α : Type) where
 abbrev SymbolicOp := Op Nat
 abbrev ConcreteOp {Object : Type} := Op Object
 
+-- Fun: Map a symbolic operation to an operation with concrete objects
 def SymbolicOp.map {Object : Type} (objects : Nat → Object) : SymbolicOp → (@ConcreteOp Object)
   | .insert i => .insert (objects i)
   | .delete i => .delete (objects i)
   | .mutate i j => .mutate (objects i) (objects j)
 
+-- Fun: Return the effects of a concrete operation
 def ConcreteOp.toEffects {Object : Type} : @ConcreteOp Object → List (@Effect Object)
   | .insert o => [.create o]
   | .delete o => [.consume o]
   | .mutate o₁ o₂ => [.consume o₁, .create o₂]
 
--- def SymbolicOp.Creates {Object : Type}
---     (op : SymbolicOp) (objects : Nat → Object) (o : Object) : Prop :=
---   match op with
---   | .insert i => objects i = o
---   | .delete _ => False
---   | .mutate _ j => objects j = o
--- 
--- def SymbolicOp.Consumes {Object : Type}
---     (op : SymbolicOp) (objects : Nat → Object) (o : Object) : Prop :=
---   match op with
---   | .insert _ => False
---   | .delete i => objects i = o
---   | .mutate i _ => objects i = o
+-- Prop: A concrete operation creates or consumes an object
+def ConcreteOp.Touches {Object : Type} : @ConcreteOp Object → Object → Prop
+  | .insert o', o => o' = o
+  | .delete o', o => o' = o
+  | .mutate o₁ o₂, o => o₁ = o ∨ o₂ = o
 
 mutual
+  -- Type: A state affecting event within an action
   inductive Event (Object : Type) where
     | operation (op : SymbolicOp)
     | subaction (a : Action Object) (mapping : Nat → Nat)
 
+  -- Type: A collection of object relations and state changes
   structure Action (Object : Type) where
     relations : List ((Nat → Object) → Prop)
     events : List (Event Object)
 end
 
+-- Type: The attempt at applying an action with concrete objects
 structure Tx (Object : Type) where
   action : Action Object
   objects : Nat → Object
 
+-- Type: The set of valid actions of an object
 structure ObjectType (Object : Type) where
   actions : Set (Action Object)
 
--- Mutation preserves object type
+-- Prop: Mutation preserves object type
 def ConcreteOp.TypePreserving {Object : Type}
   (typeOf : Object → (ObjectType Object)): (@ConcreteOp Object) → Prop
   | .mutate o₁ o₂ => (typeOf o₁) = (typeOf o₂)
   | _ => True
 
--- Reindex parent's objects through a subaction's mapping.
+-- Fun: Reindex parent's objects through a subaction's mapping.
 def reindex {Object : Type}
     (objects : Nat → Object)
     (mapping : Nat → Nat) :
     Nat → Object :=
   fun i => objects (mapping i)
 
--- mutual
---   def Event.opsLength {Object : Type}
---     (e : Event Object) : Nat :=
---     match e with
---     | .operation _ => 1
---     | .subaction a _ => a.opsLength
---   termination_by sizeOf e
---
---   def Action.opsLength {Object : Type}
---     (a : Action Object) : Nat :=
---     (a.events.attach.map fun ⟨e, _⟩ => e.opsLength).sum
---   termination_by sizeOf a
---   decreasing_by
---     rename_i h
---     have := List.sizeOf_lt_of_mem h;
---     cases a; simp_all; omega
--- end
+-- Fun: List of concrete operations that happen in an action, ignoring subactions
+def Action.directConcreteOps {Object : Type}
+  (a : Action Object) (objects : Nat → Object) : List (@ConcreteOp Object) :=
+  a.events.filterMap (fun e =>
+    match e with
+    | .operation op => some (op.map objects)
+    | .subaction _ _ => none)
 
+-- Prop: All objects touched by this action (ignoring subactions) are touched
+-- by an action in the object's type
+def Action.OpsTypeMatch {Object : Type}
+  (typeOf : Object → ObjectType Object)
+  (a : Action Object) (objects : Nat → Object) : Prop :=
+  ∀ op ∈ a.directConcreteOps objects,
+    ∀ o, op.Touches o → a ∈ (typeOf o).actions
+
+-- NOTE: These mutually recursive function definitions work on mutual recursive
+-- types.  Lean requires a proof of termination so that we can use this
+-- function in propositions.
 mutual
+  -- Fun: List of concrete operations of this event and nested actions
   def Event.concreteOps {Object : Type}
     (e : Event Object) (objects : Nat → Object) : List (@ConcreteOp Object) :=
     match e with
     | .operation op => [op.map objects]
     | .subaction a mapping => a.concreteOps (reindex objects mapping)
-  termination_by e.opsLength
+  termination_by sizeOf e
 
+  -- Fun: List of concrete operations of this action's events and nested actions
   def Action.concreteOps {Object : Type}
     (a : Action Object) (objects : Nat → Object) : List (@ConcreteOp Object) :=
     (a.events.attach.map fun ⟨e, _⟩ => e.concreteOps objects).flatten
-  termination_by a.opsLength
+  termination_by sizeOf a
+  decreasing_by
+    rename_i h
+    have := List.sizeOf_lt_of_mem h;
+    cases a; simp_all; omega
 end
 
--- theorem Action.concreteOps_length {Object : Type} (a : Action Object) (objects : Nat → Object) :
---   (a.concreteOps objects).length = a.opsLength := by
---   sorry
-
-def Action.Effects {Object : Type}
+-- Fun: List of effects of an action
+def Action.effects {Object : Type}
   (a : Action Object) (objects : Nat → Object) : List (@Effect Object) :=
   (a.concreteOps objects).flatMap (fun op => op.toEffects)
 
 
 mutual
-  -- True if P holds at every action and subaction reachable from this event
+  -- Prop: True if P holds at every action and subaction reachable from this event
   inductive Event.AllSubactions {Object : Type}
       (P : Action Object → (Nat → Object) → Prop) :
       Event Object → (Nat → Object) → Prop where
@@ -124,7 +128,7 @@ mutual
         (h_rec : Action.AllSubactions P a (reindex objects mapping)) :
         Event.AllSubactions P (Event.subaction a mapping) objects
 
-  -- True if P holds at this action and every subaction reachable from it
+  -- Prop: True if P holds at this action and every subaction reachable from it
   inductive Action.AllSubactions {Object : Type}
       (P : Action Object → (Nat → Object) → Prop) :
       Action Object → (Nat → Object) → Prop where
@@ -134,53 +138,27 @@ mutual
         Action.AllSubactions P a objects
 end
 
--- mutual
---   inductive Event.SomeOp {Object : Type}
---       (P : SymbolicOp → (Nat → Object) → Prop) :
---       Event Object → (Nat → Object) → Prop where
---     | here {objects : Nat → Object} {op : SymbolicOp}
---         (h : P op objects) :
---         Event.SomeOp P (Event.operation op) objects
---     | inSub {objects : Nat → Object}
---         (a : Action Object) (mapping : Nat → Nat)
---         (h_rec : Action.SomeOp P a (reindex objects mapping)) :
---         Event.SomeOp P (Event.subaction a mapping) objects
---
---   inductive Action.SomeOp {Object : Type}
---       (P : SymbolicOp → (Nat → Object) → Prop) :
---       Action Object → (Nat → Object) → Prop where
---     | mk {a : Action Object} {objects : Nat → Object} {e : Event Object}
---         (h_mem : e ∈ a.events)
---         (h_rec : Event.SomeOp P e objects) :
---         Action.SomeOp P a objects
--- end
-
+-- Prop: All relations in nested actions of the tx hold
 def Tx.RelationsHold {Object : Type} (tx : Tx Object) : Prop :=
   Action.AllSubactions
     (fun a objects => ∀ rel ∈ a.relations, rel objects)
     tx.action tx.objects
 
--- A Tx can't double-create nor double-consume
-def Tx.NoDoubleEffect {Object : Type} (tx : Tx Object) : Prop :=
-  (tx.action.Effects tx.objects).Nodup
-
+-- Prop: The tx creates an object at internal index i
 def Tx.CreatesAt {Object : Type} (tx : Tx Object) (o : Object) (i : Nat) : Prop :=
-  let effects := (tx.action.Effects tx.objects)
+  let effects := (tx.action.effects tx.objects)
   effects[i]? = some (Effect.create o)
 
+-- Prop: The tx creates an object at internal index i
 def Tx.ConsumesAt {Object : Type} (tx : Tx Object) (o : Object) (i : Nat) : Prop :=
-  let effects := (tx.action.Effects tx.objects)
+  let effects := (tx.action.effects tx.objects)
   effects[i]? = some (Effect.consume o)
 
--- def Tx.Creates {Object : Type} (tx : Tx Object) (o : Object) : Prop :=
---   ∃ op ∈ (tx.action.concreteOps tx.objects), .create o ∈ op.toEffects
---
--- def Tx.Consumes {Object : Type} (tx : Tx Object) (o : Object) : Prop :=
---   ∃ op ∈ (tx.action.concreteOps tx.objects), .consume o ∈ op.toEffects
-
+-- Prop: The object has been created in the history
 def InCreated {Object : Type} (o : Object) (h : List (Tx Object)) : Prop :=
   ∃ tx ∈ h, ∃ i, tx.CreatesAt o i
 
+-- Prop: The object has been consumed in the history
 def InConsumed {Object : Type} (o : Object) (h : List (Tx Object)) : Prop :=
   ∃ tx ∈ h, ∃ i, tx.ConsumesAt o i
 
@@ -188,37 +166,55 @@ def InConsumed {Object : Type} (o : Object) (h : List (Tx Object)) : Prop :=
 -- recent transaction.
 structure SystemSpec (Object : Type) where
   -- Properties that an implementation must define --
-  -- A transaction is valid to append to a history
+  -- Prop: A transaction is valid to append to a history
   ValidTx : List (Tx Object) → Tx Object → Prop
+  -- Fun: returns the type of the object
   typeOf (o : Object) : ObjectType Object
 
   -- Theorems that an implementation must prove --
 
-  -- A consumes effect is valid if
+  -- Obligation: A consumes effect is valid if
   -- * the object was previously created (in a previous tx, or in a previous effect in the tx)
   -- * the object was not previously consumed (in a previous tx, or in a previous effect in the tx)
-  validTx_consumes :
+  validTx_consume :
     ∀ h tx, ValidTx h tx →
-    let effects := (tx.action.Effects tx.objects)
+    let effects := (tx.action.effects tx.objects)
     ∀ o (i : Fin effects.length), effects[i]? = some (Effect.consume o) →
     ((InCreated o h) ∨ (∃ j < i, effects[j]? = some (Effect.create o))) ∧
     ¬ ((InConsumed o h) ∨ (∃ j < i, effects[j]? = some (Effect.consume o)))
 
-  -- A create effect is valid if
+  -- Obligation: A create effect is valid if
   -- * the object was not previously created (in a previous tx, or in a previous effect in the tx)
   validTx_create :
     ∀ h tx, ValidTx h tx →
-    let effects := (tx.action.Effects tx.objects)
+    let effects := (tx.action.effects tx.objects)
     ∀ o (i : Fin effects.length), effects[i]? = some (Effect.create o) →
     ¬ ((InCreated o h) ∨ (∃ j < i, effects[j]? = some (Effect.create o)))
 
+  -- Obligation: A mutate operation is valid if it preserves the type of the object
+  validTx_mutate :
+    ∀ h tx, ValidTx h tx →
+    let concrete_ops := (tx.action.concreteOps tx.objects)
+    ∀ op (i : Fin concrete_ops.length), concrete_ops[i]? = some op →
+    op.TypePreserving typeOf
+    -- TODO: Maybe we also want to say that the identity is preserved
+
+  -- Obligation: A transaction is valid if all the reations in actions and subactions hold
   validTx_relations_hold :
     ∀ h tx, ValidTx h tx → Tx.RelationsHold tx
+
+  -- Obligation: A transaction is valid if all touched objects are touched via operations
+  -- in actions that belong to their type
+  validTx_ops_type_match:
+    ∀ h tx, ValidTx h tx →
+    Action.AllSubactions (Action.OpsTypeMatch typeOf) tx.action tx.objects
+
 
 -- Definitions
 
 namespace SystemSpec
 
+-- Prop: The history is valid.  An sound implementation must prove this proposition.
 def ValidHistory {Object : Type} (spec : SystemSpec Object) :
     List (Tx Object) → Prop
   | [] => True
@@ -226,97 +222,29 @@ def ValidHistory {Object : Type} (spec : SystemSpec Object) :
 
 end SystemSpec
 
+-- Prop: History h happened before h'
 def Reaches  {Object : Type} (h h' : List (Tx Object)) : Prop :=  h.IsSuffix h'
 
 -- Generic theorems with proofs (for all SystemSpec)
 
+-- Theorem: If created before, created after
 theorem InCreated.mono {Object : Type} {o : Object} {h h' : List (Tx Object)} :
     Reaches h h' → InCreated o h → InCreated o h' := by
   intro hreach ⟨tx, htx_mem, htx_creates⟩
   exact ⟨tx, hreach.mem htx_mem, htx_creates⟩
 
+-- Theorem: If consumed before, consumed after
 theorem InConsumed.mono {Object : Type} {o : Object} {h h' : List (Tx Object)} :
     Reaches h h' → InCreated o h → InCreated o h' := by
   intro hreach ⟨tx, htx_mem, htx_creates⟩
   exact ⟨tx, hreach.mem htx_mem, htx_creates⟩
 
-
+-- Thereom: The genesis has no created or consumed objects
 theorem genesis_empty {Object : Type} {o : Object} :
     (¬ InCreated o []) ∧ (¬ InConsumed o []) := by
     simp [InCreated, InConsumed]
 
-/-
-structure SystemSpec (Object : Type) (State: Type) where
-  -- **Global State**
-
-  genesis : State
-
-  -- s' results from applying tx to s
-  Step (s : State) (tx : Tx Object) (s' : State) : Prop
-
-  step_deterministic (s : State) (tx : Tx Object) (s₁ s₂ : State) :
-    Step s tx s₁ → Step s tx s₂ → s₁ = s₂
-
-  -- s' is reachable from s through some sequence of transactions
-  Reaches (s s' : State) : Prop
-
-  reaches_refl (s : State) : Reaches s s
-  reaches_step (s s' : State) (tx : Tx Object) : Step s tx s' → Reaches s s'
-  reaches_trans (s s' s'' : State) :
-    Reaches s s' → Reaches s' s'' → Reaches s s''
-  -- no forks: guarantees total order of state updates
-  reaches_linear (s s' s'' : State) :
-    Reaches s s' → Reaches s s'' → Reaches s' s'' ∨ Reaches s'' s'
-
-  InCreated (o : Object) (s : State) : Prop
-  InConsumed (o : Object) (s : State) : Prop
-
-  -- Once created, always created
-  inCreated_monotone (o : Object) (s s' : State) :
-    Reaches s s' → InCreated o s → InCreated o s'
-  -- Once consumed, always consumed
-  inConsumed_monotone (o : Object) (s s' : State) :
-    Reaches s s' → InConsumed o s → InConsumed o s'
-  -- An object can only be consumed if it was created
-  consumed_implies_created (o : Object) (s : State) :
-    InConsumed o s → InCreated o s
-  genesis_empty :
-    ∀ o : Object, ¬ InConsumed o genesis ∧ ¬ InCreated o genesis
-
-  -- **Untyped Events**
-
-  UntypedInsert (o : Object) (s : State) (t : Nat) : Prop
-  UntypedDelete (o : Object) (s : State) (t : Nat) : Prop
-  UntypedMutate (o o': Object) (s : State) (t : Nat) : Prop
-
-  -- Insert: the object is in Created and not in Consumed
-  untypedInsert_prop (o : Object) (s : State) (t : Nat) :
-    UntypedInsert o s t →
-      InCreated o s t ∧ NotInConsumed o s t
-
-  -- Delete: the object was in Created and not in Consumed, afterwards it's in Consumed
-  untypedDelete_prop (o : Object) (s : State) (t : Nat) (ht : t > 0) :
-    UntypedDelete o s t →
-      InCreated o s (t-1) ∧ NotInConsumed o s (t-1) ∧ InConsumed o s t
-
-  -- Mutate: delete previous object, insert new one
-  untypedMutate_prop (o o' : Object) (s : State) (t : Nat) :
-    UntypedMutate o o' s t →
-      UntypedDelete o' s t ∧ UntypedInsert o s t
-
-  -- **Typed Events**
-
-  typeOf (o : Object) : Option (ObjectType Object)
-
-  -- Insert (o : Object) (s : State) (t : Nat) : Prop
-  -- Delete (o : Object) (s : State) (t : Nat) : Prop
-  -- Mutate (o o': Object) (s : State) (t : Nat) : Prop
-
-  -- insert_prop (o : Object) (s : State) (t : Nat) :
-  --   Insert o s t →
-  --     UntypedInsert o s t ∧
-  --     ∃ a : Action, ∃ e : TxEvent,
-  --     a ∈ (typeOf o).actions ∧
-  --     e ∈ a.tx_action.events ∧
-  --     e = TxEvent.operation TxOperation.Mutate
--/
+-- TODO: Write theorems that say: things like
+-- * There's no way to delete an object except by running an action that consumes it defined by its type
+-- * There's no way to create an object except by running an action that creates it defined by its type
+-- * There's no way to mutate an object except by running an action that mutates it defined by its type
